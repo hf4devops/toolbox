73bbdb4c04ed04088046932b7bcceb56315eaafc
hf4devops <no-reply>
2018-04-22 22:10:21 +0200

    Fixed github email

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -2,20 +2,21 @@
 
 # This script is not robust or good, just a hack. Future script will do better
 
-user="hf4devops"
+git_user="hf4devops"
+system_user="hf"
 ansible_exitcode=0
 
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible git vim
 
-git config user.name "$user"
-git config user.email "example@mail.com"
-git remote set-url origin git@github.com:$user/toolbox
+git config user.name "$git_user"
+git config user.email "no-reply"
+git remote set-url origin git@github.com:$git_user/toolbox
 
 cd ansible
 
-sudo ansible-playbook -i inventory/hf install_development_env.yaml
+sudo ansible-playbook -i inventory/$system_user install_development_env.yaml
 ansible_exitcode=$?
 
 if [[ $ansible_exitcode -eq 0 ]]; then
