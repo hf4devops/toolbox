91030343ae3441b42514711c757c9dde1b612bfc
hf4devops <h.fehlauer@gmx.de>
2018-05-06 21:15:24 +0200

    Migrated to Ubuntu 18.04

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -9,11 +9,14 @@ git config user.name "$git_user"
 git config user.email "$git_email"
 git remote set-url origin git@github.com:$git_user/toolbox
 
+sudo apt install ansible -y
+
 cd ansible
 
 sudo ansible-playbook -i inventory/$system_user install_development_env.yaml
 ansible_exitcode=$?
 
 if [[ $ansible_exitcode -eq 0 ]]; then
+	ansible-playbook -i inventory/$system_user install_non_sudo.yaml
 	/usr/local/bin/set_gnome_favorites.sh
 fi
